b7a985f74e4717edba179b38fafeb53c5964e732
Артем Стребков <guland1969@mail.ru>
2026-04-05 13:05:38 +0700

    Delete docs directory

--- docs/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('menuBtn');
-const nav = document.getElementById('nav');
-if (menuBtn && nav) {
-  menuBtn.addEventListener('click', () => nav.classList.toggle('open'));
-  nav.querySelectorAll('a').forEach(a => a.addEventListener('click', () => nav.classList.remove('open')));
-}
